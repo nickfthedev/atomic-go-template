7e16a05a49856d868ae26a1917e06516597af891
Nick Friedrich <nfriedrich1990@gmail.com>
2024-07-30 09:18:29 +0200

    auth controlflow

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,15 +1,32 @@
 package handler
 
-import "net/http"
+import (
+	"my-go-template/cmd/web/components"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
 
 func (h *Handler) HandleLogin(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Login"))
+	templ.Handler(components.SuccessResponse(components.SuccessResponseData{
+		Message:      "Login successful",
+		RedirectUrl:  &[]string{"/"}[0],
+		RedirectTime: &[]int{2}[0],
+	})).ServeHTTP(w, r)
 }
 
 func (h *Handler) HandleSignup(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Signup"))
+	templ.Handler(components.SuccessResponse(components.SuccessResponseData{
+		Message: "Signup successful. A verification email has been sent to your email address. Please verify your email address to continue.",
+		ActionButton: &components.ActionButton{
+			Label: "Login",
+			Url:   "/auth/login",
+		},
+	})).ServeHTTP(w, r)
 }
 
 func (h *Handler) HandleForgetPassword(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Forget Password"))
+	templ.Handler(components.SuccessResponse(components.SuccessResponseData{
+		Message: "A password reset email has been sent to your email address. Please check your email to reset your password.",
+	})).ServeHTTP(w, r)
 }
